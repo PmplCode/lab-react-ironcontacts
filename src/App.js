0216b0baaa85712eacc2f9b5ae07d20d54f06757
Eloi Pampliega <eloipampliega@gmail.com>
2022-10-30 15:40:01 +0100

    done, some iterations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts1 from "./contacts.json";
+import {useState} from 'react';
+
+let contactsArr = contacts1.slice(0, 5);
+let contactsArr1 = contactsArr;
+let remainingContacts = contacts1.slice(5)
 
 function App() {
+  
+  const [contacts, setContacts] = useState(contactsArr1);
+
+  
+  // let count = 5;
+
+  // const suma = () => {
+  //   console.log("Hola: ", count)
+  //   count++;
+  // }
+  
+  const afegirContact = () => {
+    
+    let newArr = contacts.map(e => {return e});
+    newArr.push(remainingContacts[Math.floor(Math.random()*remainingContacts.length)])
+    // suma();
+    setContacts(newArr)
+
+    // console.log("count: ", count)
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Iron Contacts</h1>
+    <input type="button" onClick={ afegirContact } value="Afegir Famós" />
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Has an Oscar?</th>
+          <th>Has an Emmy?</th>
+          <th>ID</th>
+        </tr>
+        {contacts.map((contact, k) => {
+          return (<tr key={k}>
+                    <td><img src={contact.pictureUrl} alt="profile"/></td>
+                    <td>{contact.name}</td>
+                    <td>{contact.popularity}</td>
+                    <td>{contact.wonOscar && "🏆"}</td>
+                    <td>{contact.wonEmmy && "🏆"}</td>
+                    <td><button >Delete</button></td>
+                  </tr>)
+        })}
+      </table>
     </div>
   );
 }
